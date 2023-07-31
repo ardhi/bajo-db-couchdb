36e18832a334763d9359479389d66dc149115cb6
Ardhi Lukianto <ardhi@lukianto.com>
2023-07-31 18:46:35 +0700

    Updates

--- bajoDb/method/record/update.js
+++ bajoDb/method/record/update.js
@@ -7,7 +7,7 @@ async function update ({ schema, id, body, options } = {}) {
   const { instance } = await getInfo(schema)
   const old = await getRecord.call(this, { schema, id, options: { thrownNotFound: true } })
   const coll = instance.client.use(schema.collName)
-  await coll.insert(merge({ _id: old.data._id, _rev: old.data._rev }, omit(old.data, ['_id', '_rev']), body))
+  await coll.insert(merge({ _id: id, _rev: old.data._rev }, omit(old.data, ['_id', '_rev']), body))
   const result = await getRecord.call(this, { schema, id, options: { thrownNotFound: true } })
   return { oldData: old.data, oldRev: old.rev, data: result.data, rev: result.rev }
 }
